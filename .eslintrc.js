e62299339527aa3b8baf6a7576b8a5fac1d38773
Japeth Naval <japeth@trunkrs.nl>
2020-08-17 15:02:19 +0800

    Baseline Setup
    prettier, eslint and editorconfig

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "parser": "@typescript-eslint/parser",
+    "extends": "airbnb-typescript-prettier",
+    "parserOptions": {
+      "ecmaVersion": 2018,
+      "sourceType": "module",
+      "ecmaFeatures": {
+        "jsx": true,
+      },
+    },
+    "env": {
+      "jest": true,
+    },
+    "rules": {
+     // This is covered by TS interfaces instead
+      "react/prop-types": 'off'
+    },
+    "settings": {
+      "react": {
+        "version": "detect"
+      }
+    }
+  };
+  
