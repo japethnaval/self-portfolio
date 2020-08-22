72d27faffe5d2ff084ae336f26485ff1e247e132
Japeth Naval <japeth@trunkrs.nl>
2020-08-22 16:43:49 +0800

    Updates

---
+++ precache-manifest.c5bc5f39403463e420015ade43e0cf67.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfbe43fa20dd75a26c4f9d6169fc83e1",
+    "url": "/self-portfolio/index.html"
+  },
+  {
+    "revision": "ebe066780fcfc8474232",
+    "url": "/self-portfolio/static/css/2.5568c9f7.chunk.css"
+  },
+  {
+    "revision": "326443af29032d5a54d3",
+    "url": "/self-portfolio/static/css/main.c7af90f8.chunk.css"
+  },
+  {
+    "revision": "ebe066780fcfc8474232",
+    "url": "/self-portfolio/static/js/2.6828e080.chunk.js"
+  },
+  {
+    "revision": "398950697d386f55350dbba37820732c",
+    "url": "/self-portfolio/static/js/2.6828e080.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "326443af29032d5a54d3",
+    "url": "/self-portfolio/static/js/main.b97bfaed.chunk.js"
+  },
+  {
+    "revision": "b958926ac1108e2501d1",
+    "url": "/self-portfolio/static/js/runtime-main.a0052aee.js"
+  },
+  {
+    "revision": "47004c68af2e89cea05b17ee5db0d980",
+    "url": "/self-portfolio/static/media/aws_logo.47004c68.png"
+  },
+  {
+    "revision": "ce280c02a6a84fa771e3fa448b214d2a",
+    "url": "/self-portfolio/static/media/background.ce280c02.jpg"
+  },
+  {
+    "revision": "cd0c43b19fee586d1e51315d8808bad4",
+    "url": "/self-portfolio/static/media/background_2.cd0c43b1.jpg"
+  },
+  {
+    "revision": "17204d17fd64f51aa0a01f701b16e1a5",
+    "url": "/self-portfolio/static/media/html_logo.17204d17.png"
+  },
+  {
+    "revision": "4fae32356ec7c2a32ca53a5bc2e92f56",
+    "url": "/self-portfolio/static/media/jap.4fae3235.jpg"
+  },
+  {
+    "revision": "c9d39006512a8aba111a100baae5ad6a",
+    "url": "/self-portfolio/static/media/redux_logo.c9d39006.png"
+  }
+]);
